1167df06dd90377521ea1b5c93565d25c15012a7
Sai Gautham Kolluru <gautham.kolluru@gmail.com>
2018-10-11 08:18:19 +0530

    todos.py

---
+++ todos.py
@@ -0,0 +1,15 @@
+# TODO: Hopefully by 14th October
+"""
+* - [] create an application that collects the ip address of the website visitors and finds details about the visitor.
+* - [] javascript application that does the same as above
+* - [] complete creating DB for DL by Sunday 14th Oct 2018
+* - [] implement Queues using Lists in python
+* - [] Learn Unit Testing in Python and move to Selenium automated testing in python
+* - [] change all the scripts written until now to implement testing
+* - [] Script for installing modules that are not in the existing environment
+"""
+
+# FIXME: Hopefully by 14th October
+"""
+* - [] Existing script for installing modules that are not in the existing environment must be modified
+"""
