ba6d79e87748960289026313e4f00f75d99341ab
Gautham <gautham.kolluru@gmail.com>
2019-08-28 12:14:02 +0530

    updates until 27th August 2019

--- pyscripts/password_keeper.py
+++ pyscripts/password_keeper.py
@@ -19,7 +19,7 @@ def write_pickle(pl):
         pickle.dump(pl, fp)
     return 'Done'
 
-def new_password(cipsut, site_name,site_user,site_pass):
+def new_password(cipsut, site_name, site_user, site_pass):
     pl = read_pickle()
     if site_name and site_name in pl:
         pl[site_name].update({site_user:site_pass})
@@ -27,33 +27,36 @@ def new_password(cipsut, site_name,site_user,site_pass):
         pl.update({site_name:{site_user:site_pass}})
     return write_pickle(pl)
 
-def del_password(cipsut, site_name,site_user):
-    pl = read_pickle()
-    if site_name and site_name in pl:
-        if site_user and site_user in pl[site_name] and len(pl[site_name]) > 1:
-            pl[site_name].pop(site_user)
-        else:
-            pl.pop(site_name)
-    else:
-        print('Sorry! Entered details does not exist !!!')
-    return write_pickle(pl)
-
 def show_password(cipsut, site_name,site_user):
     pl = read_pickle()
     if len(pl) < 1:
         print('File is empty!')
-    elif site_name and site_name in pl:
+        return 'Done'
+    if site_name and site_name in pl:
         if site_user and site_user in pl[site_name]:
-            print(cipsut.decrypt(pl[site_name][site_user]))
+            print(cipsut.decrypt(site_name),cipsut.decrypt(site_user),cipsut.decrypt(pl[site_name][site_user]))
         else:
             for i in pl[site_name]:
-                print(cipsut.decrypt(i), cipsut.decrypt(pl[site_name][i]))
+                print(cipsut.decrypt(site_name),cipsut.decrypt(i), cipsut.decrypt(pl[site_name][i]))
     else:
         for i in pl:
             for j in pl[i]:
-                print(cipsut.decrypt(j),cipsut.decrypt(pl[i][j]))
+                print(cipsut.decrypt(i),cipsut.decrypt(j),cipsut.decrypt(pl[i][j]))
     return 'Done'
 
+def del_password(cipsut, site_name, site_user):
+    pl = read_pickle()
+    print(site_name)
+    for i in pl:
+        if site_name and cipsut.decrypt(site_name) == cipsut.decrypt(i):
+            print('hi')
+            if site_user and cipsut.decrypt(site_user) in pl[site_name] and len(pl[site_name]) > 1:
+                pl[site_name].pop(site_user)
+            else:
+                pl.pop(site_name)
+        else:
+            print('Sorry! Entered details does not exist !!!')
+    return write_pickle(pl)
 
 def selected_choice(user_choice):
     cipsut = user_key()
