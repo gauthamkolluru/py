e76d170060ca1a7ec8cd4d9607debed77451cd46
Gautham <gautham.kolluru@gmail.com>
2019-10-17 19:22:53 +0530

    updated folder-destroyer.py

--- Automation/folder-destroyer.py
+++ Automation/folder-destroyer.py
@@ -12,12 +12,12 @@ fl = [
 
 def remove_empty_folders(folder_path):
     if os.path.isdir(folder_path):
-        if os.listdir(folder_path):
+        if not os.listdir(folder_path):
+            return os.rmdir(folder_path)
+        else:
             for abp, folder_list, rlp in os.walk(folder_path):
                 for each_dir in folder_list:
                     remove_empty_folders(os.path.join(abp, each_dir))
-        else:
-            return os.rmdir(folder_path)
 
 for f in fl :
     remove_empty_folders(f)
