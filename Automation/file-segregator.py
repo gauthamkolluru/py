d9853d6c776b6a89fb83ce7020c4d0b4ecbe89c4
Gautham <gautham.kolluru@gmail.com>
2019-10-20 21:09:47 +0530

    updated file-segregator.py

--- Automation/file-segregator.py
+++ Automation/file-segregator.py
@@ -21,22 +21,23 @@ destination_folders = {
 
 # We os.walk() method to parse through the downloads folder and get the details of all the files
 
-# abp (Absolute Paths) = absolute path of the source folder
+# abs_path (Absolute Paths) = absolute path of the source folder
 
-# ff (files / folders) = list of files / folders in the current path
+# sub_dirs (files / folders) = list of files / folders in the current path
 
-# rlp (Relative Paths) = list of relative paths of each of the files in the current and it's sub folders
+# rel_path (Relative Paths) = list of relative paths of each of the files in the current and it's sub folders
 
 # => combining abp & rlp will will the absolute path of each file in the source folder and it's sub-folders
 
-for abp,ff,rlp in os.walk(source_folder):
-    for each_file in rlp:
+for abs_path,sub_dirs,file_names in os.walk(source_folder):
+    # print(abs_path, sub_dirs, file_names)
+    for each_file in file_names:
         folder_key = ''
-        for extention in destination_folders.keys():
+        for extention in destination_folders:
             if not folder_key:
                 folder_key = extention if each_file.split('.')[-1].lower() in extention else ''
         if folder_key:
-            shutil.move(os.path.join(abp, each_file), os.path.join(destination_folders[folder_key], each_file))
+            shutil.move(os.path.join(abs_path, each_file), os.path.join(destination_folders[folder_key], each_file))
         #     print(each_file + ' has been moved to ' + destination_folders[folder_key])
         # else:
         #     print("folder key did not exist" + each_file)
