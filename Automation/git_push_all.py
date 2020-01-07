409959f112f50477f53adfb8649325a015a5e3cc
Gautham Kolluru <gautham.kolluru@gmail.com>
2020-01-07 20:17:34 +0530

    updated git_push_all.py

--- Automation/git_push_all.py
+++ Automation/git_push_all.py
@@ -3,28 +3,30 @@ import sys
 import subprocess
 
 
-def set_root_dir():
-    if sys.platform == 'win32':
-        return f'C:\\Users\\{os.environ["USERNAME"]}\\Documents'
-    return f'/home/{os.environ["USER"]}/Documents/'
+class GitPullPush:
+    def set_root_dir(self):
+        if sys.platform == 'win32':
+            return f'C:\\Users\\{os.environ["USERNAME"]}\\Documents'
+        return f'/home/{os.environ["USER"]}/Documents/'
 
 
-def find_local_repos(root_dir):
-    return [directory for directory in os.listdir(root_dir) if os.path.exists(
-        os.path.join(os.path.join(root_dir, directory), '.git'))]
+    def find_local_repos(self, root_dir):
+        return [directory for directory in os.listdir(root_dir) if os.path.exists(
+            os.path.join(os.path.join(root_dir, directory), '.git'))]
 
-def git_pull_all(dir_list):
-    for directory in dir_list:
-        subprocess.run(['cd', os.path.join(set_root_dir(), directory)], shell=True)
-        subprocess.run(['git','pull'], shell=True)
-    return "ok"
+    def git_pull_all(self, dir_list):
+        for directory in dir_list:
+            subprocess.run(['cd', os.path.join(self.set_root_dir(), directory)], shell=True)
+            subprocess.run(['git','pull'], shell=True)
+        return "ok"
 
-def find_modified_repos(dir_list):
-    for directory in dir_list:
-        dir_contents = os.listdir(os.path.join(set_root_dir(),directory))
-        for content in dir_contents:
-            pass
-    return ""
+    def find_modified_repos(self, dir_list):
+        for directory in dir_list:
+            for content in os.listdir(os.path.join(self.set_root_dir(),directory)):
+                pass
+        return ""
 
 
-git_pull_all(find_local_repos(set_root_dir()))
+if __name__ == "__main__":
+    git_obj = GitPullPush()
+    git_obj.git_pull_all(git_obj.find_local_repos(git_obj.set_root_dir()))
